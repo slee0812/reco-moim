8550c9f8ab365a9c65dd198fac0c33dc9d75e5f4
MyeongGil Jeong <itsstylemk@gmail.com>
2024-12-16 14:21:45 +0900

    modify map sample code

--- Code/maps/Flask.py
+++ Code/maps/Flask.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import requests
+from FindDestination import calculate_centroid
 
 app = Flask(__name__, template_folder='../frontend')
 
@@ -9,15 +10,32 @@ API_KEY = "4f040348a11373f7f6d1cdae6778fd0f"
 def index():
     return render_template("test.html")
 
+@app.route("/centroid")
+def get_centroid():
+    # 지하철역들에 대한 좌표 (예시 데이터)
+    subway_stations = [
+        (37.4019, 126.9229),
+        (37.3947, 126.9631),
+        (37.4842, 126.9293)
+    ]
+    # 중심점 계산
+    latitude, longitude = calculate_centroid(subway_stations)
+    
+    # centroid 값을 반환
+    return jsonify({
+        "latitude": latitude,
+        "longitude": longitude
+    })
+
 @app.route("/places")
 def get_places():
-    # 카카오 REST API 호출
+    latitude, longitude = calculate_centroid([(37.4019, 126.9229), (37.3947, 126.9631), (37.4842, 126.9293)])
     url = "https://dapi.kakao.com/v2/local/search/keyword.json"
     params = {
-        "query": "스타벅스",
-        "x": "126.9784147",
-        "y": "37.5666805",
-        "radius": 2000,
+        "query": "지하철역",
+        "x": longitude,
+        "y": latitude,
+        "radius": 5000,
     }
     headers = {"Authorization": f"KakaoAK {API_KEY}"}
     response = requests.get(url, headers=headers, params=params)
